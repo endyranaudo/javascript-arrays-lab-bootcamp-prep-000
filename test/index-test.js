3fd03aaeee95a0c365705edc74f1cb2969c81e9a
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 14:19:48 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -57,7 +57,7 @@ describe('Arrays', function() {
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
     })
   })
-
+// ##down## //
   describe('removeLastKitten()', function() {
     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
