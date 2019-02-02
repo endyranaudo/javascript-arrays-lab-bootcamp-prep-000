802d16fb5be9434cfe25bda71d736c3edfd18062
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 10:50:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(name){
+function destructivelyAppendKitten(){
   kittens.push("Ralph")
 }
 
-function destructivelyPrependKitten(name){
+function destructivelyPrependKitten(){
   kittens.unshift("Bob")
 }
 
-function destructivelyRemoveLastKitten(name){
+function destructivelyRemoveLastKitten(){
   kittens.pop();
 }
 
